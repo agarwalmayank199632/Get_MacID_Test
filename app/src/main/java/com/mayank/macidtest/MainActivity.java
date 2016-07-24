309ce8e12284a744fb5b7d59675bc306ddab2e19
Mayank Agarwal <agarwal.mayank32@gmail.com>
2016-07-24 21:01:19 +0530

    Initial commit

---
+++ app/src/main/java/com/mayank/macidtest/MainActivity.java
@@ -0,0 +1,57 @@
+package com.mayank.macidtest;
+
+import android.content.Context;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import java.net.NetworkInterface;
+import java.util.Collections;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //WifiManager wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        //WifiInfo wInfo = wifiManager.getConnectionInfo();
+        //Toast.makeText(MainActivity.this,wInfo.getMacAddress(),Toast.LENGTH_SHORT).show();
+        Toast.makeText(MainActivity.this,getMacAddr(),Toast.LENGTH_LONG).show();
+
+    }
+
+    public String getMacAddr() {
+        try {
+            List<NetworkInterface> all = Collections.list(NetworkInterface.getNetworkInterfaces());
+            for (NetworkInterface nif : all) {
+                if (!nif.getName().equalsIgnoreCase("wlan0")) continue;
+
+                byte[] macBytes = nif.getHardwareAddress();
+                if (macBytes == null) {
+                    return "";
+                }
+
+                StringBuilder res1 = new StringBuilder();
+                for (byte b : macBytes) {
+                    res1.append(String.format("%02X:",b));
+                }
+
+                if (res1.length() > 0) {
+                    res1.deleteCharAt(res1.length() - 1);
+                }
+                return res1.toString();
+            }
+        } catch (Exception ex) {
+        }
+
+        WifiManager wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        WifiInfo wInfo = wifiManager.getConnectionInfo();
+        return wInfo.getMacAddress();
+    }
+
+}
